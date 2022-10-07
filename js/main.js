8fca55f2893b62aecaac58585695c9f1f4028d88
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2022-10-07 15:58:46 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 let menuBtn = document.querySelector(".header__burger-mobile");
+let menu = document.querySelector(".mobile__nav");
+let wrap = document.querySelector(".wrap");
 menuBtn.addEventListener("click", function(){ menuBtn.classList.toggle("active");
+menu.classList.toggle("active");
+wrap.classList.toggle("active")                                             
 })
