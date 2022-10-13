88773196c00519c2a5a0c0da699a9886efb4dea6
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2022-10-13 14:05:45 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let menuBtn = document.querySelector(".header__burger-mobile");
-let menu = document.querySelector(".mobile__nav");
-let wrap = document.querySelector(".wrap");
-menuBtn.addEventListener("click", function(){ menuBtn.classList.toggle("active");
-menu.classList.toggle("active");
-wrap.classList.toggle("wrap__active")                                             
-})
