0c957b0238355d0fc3ae403a6b48925859f56ac3
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2022-11-30 00:04:06 +0300

    Add files via upload

---
+++ js/call.js
@@ -0,0 +1,13 @@
+// CallBack
+
+let activeCallAll = document.querySelector(".back-call");
+const modal = document.querySelector(".callback");
+const closeCall = document.querySelector(".callback__close");
+
+activeCallAll.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeCall.onclick = function () {
+  modal.style.display = "none";
+};
