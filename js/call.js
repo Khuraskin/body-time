461cdefc55d84eae25a1c5b42a81000227f3981d
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2022-11-30 00:48:17 +0300

    Update call.js

--- js/call.js
+++ js/call.js
@@ -1,3 +1,14 @@
+//Menu Burger Header
+
+let menuBtn = document.querySelector(".header__burger-mobile");
+let menu = document.querySelector(".mobile__nav");
+let wrap = document.querySelector(".wrap");
+menuBtn.addEventListener("click", function () {
+  menu.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+  wrap.classList.toggle("wrap__active");
+});
+
 // CallBack
 
 let activeCallAll = document.querySelector(".back-call");
