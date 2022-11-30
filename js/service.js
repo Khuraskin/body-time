26d873307ce97a8b3c1936ca241081d0d07ed2c9
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2022-11-30 17:16:18 +0300

    Update service.js

--- js/service.js
+++ js/service.js
@@ -1,8 +1,8 @@
-function  init() {
-let map = new ymap.Map("map__ya", {
-  center: [55.03143602203169,82.91512407412706],
-zoom:18
-});
-}
-
-ymaps.ready(init);
+ let myMap;
+const init = () => {
+ myMap = new ymaps.Map("map__ya", {
+   center: [55.03143602203169,82.91512407412706],
+   zoom: 11,
+   controls: [],
+ });
+ 
